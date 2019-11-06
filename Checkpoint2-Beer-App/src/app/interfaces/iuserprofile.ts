6b5a9a8fc36682e986d4f8043d7135c9a002379c
TristanHoladay <tholaday777@gmail.com>
2019-11-05 20:39:02 -0600

    creating user profile component

---
+++ Checkpoint2-Beer-App/src/app/interfaces/iuserprofile.ts
@@ -0,0 +1,10 @@
+export interface IUserProfile {
+    user: object;
+    picture: string;
+    DOB: string;
+    homeAddress: string;
+    favoriteFood: string;
+    favoriteMovie: string;
+    favoriteArtist: string;
+    hobbies: string[];
+}
--- Checkpoint2-Beer-App/src/app/userprofile/userprofile.component.ts
+++ Checkpoint2-Beer-App/src/app/userprofile/userprofile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { IUser } from '../interfaces/user';
 
 @Component({
   selector: 'app-userprofile',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./userprofile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-
-  constructor() { }
+  profileForm: FormGroup;
+  user: IUser;
+  edit: boolean;
+  
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.profileForm = this.formBuilder.group({
+      picURL: new FormControl(),
+      dob: new FormControl(),
+      address: new FormControl(),
+      favFood: new FormControl(),
+      favMovie: new FormControl(),
+      favArtist: new FormControl(),
+      interests: new FormControl()
+    });
+    this.user = this.authService.currentUserValue;
   }
 
 }
