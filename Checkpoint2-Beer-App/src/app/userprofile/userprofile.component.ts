837a4baff894aa9fc632cad940b30c334b7576ec
TristanHoladay <tholaday777@gmail.com>
2019-11-07 20:12:05 -0600

    working on property binding

--- Checkpoint2-Beer-App/src/app/userprofile/userprofile.component.ts
+++ Checkpoint2-Beer-App/src/app/userprofile/userprofile.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { IUser } from '../interfaces/user';
+import { Router } from '@angular/router';
+import { IUserProfile } from '../interfaces/iuserprofile';
 
 @Component({
   selector: 'app-userprofile',
@@ -11,17 +13,19 @@ import { IUser } from '../interfaces/user';
 export class UserProfileComponent implements OnInit {
   profileForm: FormGroup;
   user: IUser;
-  edit: boolean;
-  
+  userProfile: IUserProfile;
+  edit: boolean = false;
+
   constructor(
     private authService: AuthService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.profileForm = this.formBuilder.group({
       picURL: new FormControl(),
-      dob: new FormControl(),
+      dob: new FormControl('03/14/1990'),
       address: new FormControl(),
       favFood: new FormControl(),
       favMovie: new FormControl(),
@@ -29,6 +33,21 @@ export class UserProfileComponent implements OnInit {
       interests: new FormControl()
     });
     this.user = this.authService.currentUserValue;
+    console.log(this.user.firstName);
+
+    window.alert("You need to fill out your profile!");
+  }
+
+  onSubmit(profile: any) {
+    console.log("Submit Successful. Send Data to Database");
+    this.edit = false;
+    this.userProfile.DOB = profile.dob;
+  }
+
+  logout() {
+    this.authService.logout;
+    window.alert("goodbye");
+    this.router.navigateByUrl("login");
   }
 
 }
